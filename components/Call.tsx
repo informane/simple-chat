c4dcaf89c38336ca6c189dabab37224d6ea58400
infro <infromane@gmail.com>
2026-02-12 13:19:56 +0200

    block call

--- components/Call.tsx
+++ components/Call.tsx
@@ -24,7 +24,7 @@ function Call(props: { appId: string; channelName: string, rtcToken: string, num
       <Videos channelName={props.channelName} AppID={props.appId} token={props.rtcToken} uid={props.numericUid} />
       <div className="fixed z-10 bottom-0 left-0 right-0 flex justify-center pb-4">
         <a
-          className="px-5 py-3 float-right text-base font-medium text-center text-white bg-red-400 rounded-lg hover:bg-red-500 focus:ring-4 focus:ring-blue-300 dark:focus:ring-blue-900 w-40"
+          className="px-5 py-3 block float-right text-base font-medium text-center text-white bg-red-400 rounded-lg hover:bg-red-500 focus:ring-4 focus:ring-blue-300 dark:focus:ring-blue-900 w-40"
           href="/"
         >
           End Call
